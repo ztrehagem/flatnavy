e473b177f6552ee23cf4d349160b1b3fc3c1bf80
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2023-04-23 03:10:05 +0900

    hide PostForm if loggedOut

--- frontend/src/ui/MainView/MainView.tsx
+++ frontend/src/ui/MainView/MainView.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import * as css from "./MainView.css.js";
 import { PostForm } from "../PostForm/PostForm.jsx";
 import { TimelineView } from "../TimelineView/TimelineView.jsx";
+import { useSession } from "../../model/Session/useSession.js";
 
 export const MainView: React.FC = () => {
+  const session = useSession();
+
   return (
     <div className={css.root}>
-      <PostForm />
+      {session && <PostForm />}
 
       <TimelineView />
     </div>
