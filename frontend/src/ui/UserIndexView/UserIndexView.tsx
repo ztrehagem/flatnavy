bcb525d1d815c9fdcf581ad4ed2c8e0392bc0452
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2023-04-27 22:41:56 +0900

    user list

--- frontend/src/ui/UserIndexView/UserIndexView.tsx
+++ frontend/src/ui/UserIndexView/UserIndexView.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as css from "./UserIndexView.css.js";
+import type { schemas } from "@flatnavy/api";
+import { indexUser } from "@flatnavy/api/client";
+import { apiClientContext } from "../../lib/api.js";
 
 export const UserIndexView: React.FC = () => {
-  // const [, users = []] = await indexUser(apiClientContext)();
-  const users = ["foo", "bar"];
+  const [users, setUsers] = useState<Array<schemas["User"]>>([]);
+
+  useEffect(() => {
+    void indexUser(apiClientContext)().then(([, users = []]) => {
+      setUsers(users);
+    });
+
+    return () => {
+      setUsers([]);
+    };
+  }, []);
 
   return (
     <div className={css.root}>
       <ul className={css.list}>
         {users.map((user) => (
-          <li key={user}>
-            <div>{user}</div>
-            <div>@{user}</div>
+          <li key={user.handle}>
+            <div>{user.name}</div>
+            <div>@{user.handle}</div>
           </li>
         ))}
       </ul>
