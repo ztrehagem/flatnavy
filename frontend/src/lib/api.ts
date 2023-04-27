fa2f09cdd3fbe55948bd12cfc925a3317f1a7eab
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2023-04-27 23:19:05 +0900

    fix sw

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ import type { ApiClientContext } from "@flatnavy/api/client";
 
 export const apiOrigin = import.meta.env.DEV
   ? "http://localhost:3000"
-  : window.location.origin;
+  : location.origin;
 
 export const apiClientContext: ApiClientContext = {
   origin: apiOrigin,
